ab0706493bad56e501045ea10c9cab841753b1d5
R.J. LaCount <rjlacount@gmail.com>
2020-01-24 14:24:28 -0800

    Link 404 page home

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,10 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
 	<Layout>
 		<SEO title="404: Not found" />
-		<h1>NOT FOUND</h1>
-		<p>Just go home.</p>
+		<div class="p-4 text-center">
+			<h1 class="text-5xl font-primary tracking-wide text-4xl">Not found</h1>
+			<p><a class="font-bold font-secondary underline text-blue-600 hover:no-underline" href="/">Just go home.</a></p>
+		</div>
 	</Layout>
 )
 
