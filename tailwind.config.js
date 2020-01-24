d6bfdc44c01f7bdbc5bc6b42277bb3958ddfe516
R.J. LaCount <rjlacount@gmail.com>
2020-01-24 14:08:53 -0800

    Override Tailwind color defaults

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,57 +1,57 @@
 module.exports = {
 	theme: {
-		extend: {
-			colors: {
-				transparent: 'transparent',
+		colors: {
+			transparent: 'transparent',
 
-				black: '#000',
-				white: '#fff',
+			black: '#000',
+			white: '#fff',
 
-				gray: {
-					100: '#f7fafc',
-					200: '#edf2f7',
-					300: '#e2e8f0',
-					400: '#cbd5e0',
-					500: '#a0aec0',
-					600: '#718096',
-					700: '#4a5568',
-					800: '#2d3748',
-					900: '#1a202c',
-				},
-
-				blue: {
-					400: '#D6E5FE',
-					600: '#3D72C6',
-				},
-
-				brown: {
-					400: '#F2E0CA',
-					600: '#8D5518',
-				},
-
-				green: {
-					400: '#D7F4B5',
-					600: '#508C17',
-				},
-
-				orange: {
-					400: '#FEDFB9',
-					600: '#D0761D',
-				},
-
-				purple: '#EFD9FE',
-
-				red: {
-					400: '#FED6EA',
-					600: '#D93180',
-				},
-
-				teal: {
-					400: '#ACFBFE',
-					600: '#16959B',
-				},
-
+			gray: {
+				100: '#f7fafc',
+				200: '#edf2f7',
+				300: '#e2e8f0',
+				400: '#cbd5e0',
+				500: '#a0aec0',
+				600: '#718096',
+				700: '#4a5568',
+				800: '#2d3748',
+				900: '#1a202c',
 			},
+
+			blue: {
+				400: '#D6E5FE',
+				600: '#3D72C6',
+			},
+
+			brown: {
+				400: '#F2E0CA',
+				600: '#8D5518',
+			},
+
+			green: {
+				400: '#D7F4B5',
+				600: '#508C17',
+			},
+
+			orange: {
+				400: '#FEDFB9',
+				600: '#D0761D',
+			},
+
+			purple: '#EFD9FE',
+
+			red: {
+				400: '#FED6EA',
+				600: '#D93180',
+			},
+
+			teal: {
+				400: '#ACFBFE',
+				600: '#16959B',
+			},
+
+		},
+		extend: {
 			fontFamily: {
 				primary: [
 					'Bebas Neue',
